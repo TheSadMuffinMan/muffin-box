7dcd998b7a00b3af50530c17d55381004aaaab2b
muffins <albusgithub@mevacy.com>
2026-05-20 10:41:23 -0600

    ts environment setup

--- typescript_learning/1-hello-world/index.ts
+++
@@ -1,4 +0,0 @@
-// Basic console print:
-// console.log('Hello World.');
-
-let age:number = 28; // 'age' is now a number.
---
+++ typescript_learning/1-hello-world/src/index.ts
@@ -0,0 +1,4 @@
+// Basic console print:
+// console.log('Hello World.');
+
+let age:number = 28; // 'age' is now a number. age = 'a'; Line doesn't work: "Type 'string' is not assignable to type 'number'." TS catches this error before compilation!
