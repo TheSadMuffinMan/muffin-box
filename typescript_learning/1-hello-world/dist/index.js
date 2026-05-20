92492e8e34ed9ca5a6deb1080e0facf8196c37f9
muffins <albusgithub@mevacy.com>
2026-05-20 11:04:17 -0600

    ts and js types

--- typescript_learning/1-hello-world/dist/index.js
+++ typescript_learning/1-hello-world/dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 let age = 28;
+if (age < 50)
+    age += 10;
 //# sourceMappingURL=index.js.map
--- typescript_learning/1-hello-world/src/index.ts
+++ typescript_learning/1-hello-world/src/index.ts
@@ -1,4 +1,29 @@
 // Basic console print:
 // console.log('Hello World.');
 
-let age:number = 28; // 'age' is now a number. age = 'a'; Line doesn't work: "Type 'string' is not assignable to type 'number'." TS catches this error before compilation!
+// let age:number = 28; // 'age' is now a number. age = 'a'; Line doesn't work: "Type 'string' is not assignable to type 'number'." TS catches this error before compilation!
+
+/* 
+JS (and therefore TS) has several native types:
+- number
+- string
+- boolean
+- null
+- undefined
+- object
+*/
+
+/*
+TS introduces a few new types:
+- any
+- unknown
+- never
+- enum
+- tuple
+*/
+
+let sales:number = 123_456_789; // Can remove `:number` section, compiler can infer data types. 
+let course:string = 'TypeScript';
+let is_published:boolean = false;
+
+let level; // Type `any`: can represent any type of values. This feature removes all the benefits of TS, so use sparingly.
